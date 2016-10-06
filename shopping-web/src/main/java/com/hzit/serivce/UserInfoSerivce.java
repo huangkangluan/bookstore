00b271d7d8ff6b7cff28541159216eadfe76c796
huangkangluan <853854757@qq.com>
2016-10-06 16:04:39 +0800

    黄康銮

---
+++ shopping-web/src/main/java/com/hzit/serivce/UserInfoSerivce.java
@@ -0,0 +1,7 @@
+package com.hzit.serivce;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public interface UserInfoSerivce {
+}
---
+++ shopping-web/src/main/java/com/hzit/serivce/impl/UserInfoSerivceImpl.java
@@ -0,0 +1,9 @@
+package com.hzit.serivce.impl;
+
+import com.hzit.serivce.UserInfoSerivce;
+
+/**
+ * Created by Administrator on 2016/10/6.
+ */
+public class UserInfoSerivceImpl implements UserInfoSerivce{
+}
