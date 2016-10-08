00df4b1ad3f6e2184cc104c97deb58fc80c02dd1
huangkangluan <853854757@qq.com>
2016-10-08 17:32:18 +0800

    黄康銮

---
+++ shopping-web/src/main/java/com/hzit/service/OrderService.java
@@ -0,0 +1,10 @@
+package com.hzit.service;
+
+import com.hzit.dao.vo.OrderVo;
+
+/**
+ * Created by Administrator on 2016/10/8.
+ */
+public interface OrderService {
+    public boolean addOrder(OrderVo orderVo);
+}
