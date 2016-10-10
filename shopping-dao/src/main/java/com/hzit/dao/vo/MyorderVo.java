7d00933539f4f2dab378626ffc293fee73b316d9
huangkangluan <853854757@qq.com>
2016-10-10 09:04:03 +0800

    黄康銮

--- shopping-dao/src/main/java/com/hzit/dao/vo/MyorderVo.java
+++ shopping-dao/src/main/java/com/hzit/dao/vo/MyorderVo.java
@@ -37,14 +37,13 @@ public class MyorderVo {
 	 *  订单状态
 	 */
 	private String myOrderStatus;
-	private List<String> bookImage;
+	private List<BookVo> bookImage;
 
-
-	public List<String> getBookImage() {
+	public List<BookVo> getBookImage() {
 		return bookImage;
 	}
 
-	public void setBookImage(List<String> bookImage) {
+	public void setBookImage(List<BookVo> bookImage) {
 		this.bookImage = bookImage;
 	}
 
